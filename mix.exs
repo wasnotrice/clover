d84d73bbf21cabb0542e41bb768cbcce7b6d6467
Eric Watson <wasnotrice@gmail.com>
2018-10-15 12:57:52 -0500

    Prep for hex release

--- mix.exs
+++ mix.exs
@@ -1,10 +1,15 @@
 defmodule Clover.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
-      version: "0.1.0",
       app: :clover,
+      version: @version,
+      name: "Clover",
+      description: description(),
+      package: package(),
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -12,16 +17,12 @@ defmodule Clover.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [extra_applications: [:logger], mod: {Clover, []}, registered: [Clover]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:gen_state_machine, "~> 2.0"}
     ] ++ adapter_deps()
   end
@@ -29,6 +30,19 @@ defmodule Clover.MixProject do
   def adapter_deps do
     _slack = [
       {:slack, "~> 0.14.0"}
+    ]
+  end
+
+  def description do
+    """
+    A framework for building chat bots
+    """
+  end
+
+  def package do
+    [
+      licenses: ["Apache 2.0"],
+      maintainers: ["Eric Watson"]
     ]
   end
 
