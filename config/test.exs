d310c3156a3afef12a981707cd9a8a74ece321d6
Eric Watson <wasnotrice@gmail.com>
2018-10-17 11:58:47 -0500

    Setup junit report for circleci

--- config/test.exs
+++ config/test.exs
@@ -1,2 +1,5 @@
 use Mix.Config
 config :logger, level: :error
+
+config :junit_formatter,
+  report_dir: Path.join(Mix.Project.app_path(), "ex_unit")
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,3 @@
+File.mkdir_p!(Application.app_dir(:clover, "ex_unit"))
 ExUnit.configure(formatters: [JUnitFormatter, ExUnit.CLIFormatter])
 ExUnit.start()
